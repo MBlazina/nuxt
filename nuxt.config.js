e19d98b6d898f3b2eb2af2ce7fc0b0cbfa8c8ce8
MBlazina <mirko.blazina@gmail.com>
2020-08-09 22:34:06 +0200

    add gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  router: {
+    base: '/nuxt/',
+  }
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
